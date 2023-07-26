fd0876c9f936c3c70914af91bd9d929251102471
vivekappadurai07 <138948227+vivekappadurai07@users.noreply.github.com>
2023-07-26 15:07:06 -0700

    Update streamlit_app.py
    Updated Menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,7 @@ streamlit.title('My Parents New Healthy Diner')
 
 streamlit.header('Breakfast Menu')
 
-streamlit.text('Mushroom Omlette - 10 USD')
+streamlit.text('Mushroom Omlette')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
