93138e8aa7768c862ac930c6c98e3b83617efc2a
vivekappadurai07 <138948227+vivekappadurai07@users.noreply.github.com>
2023-07-26 15:09:03 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,3 +8,5 @@ streamlit.text(' 🐔 Mushroom Omlette')
 streamlit.text(' 🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('  🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
