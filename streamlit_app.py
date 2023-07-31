3b5f25e5d95d352787829ba7819be0e38e1f3adc
vivekappadurai07 <138948227+vivekappadurai07@users.noreply.github.com>
2023-07-31 16:27:59 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ streamlit.dataframe(fruits_to_show)
 
 #New Section to display fruity vice advice 
 
-def get_fruity_vice_data(this_fruit_choice)
+def get_fruity_vice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     return fruityvice_normalized
