0590fdad1ed2931d2d023e27e4018c2f4faf79cb
vivekappadurai07 <138948227+vivekappadurai07@users.noreply.github.com>
2023-07-31 14:54:08 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import pandas
+
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -10,7 +12,6 @@ streamlit.text('  🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text(' 🐔 Hard-Boiled Free-Range Egg')
 
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
@@ -29,4 +30,8 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 
+# Normalize the JSON
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+# Display the Normalized results
+streamlit.dataframe(fruityvice_normalized)
 
